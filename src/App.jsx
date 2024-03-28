3994b9600468b6772d7b1ca11415aea3423c4165
Orangel Valdespino <orangelvc93@gmail.com>
2024-03-28 15:08:46 -0500

    configurando server mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Banner } from "./components/Banner";
 import NavBar from "./components/NavBar";
 
 function App() {
 	return (
 		<>
 			<NavBar />
+			<section className="container">
+				<Banner />
+			</section>
 		</>
 	);
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Banner.css";
+
+export const Banner = () => {
+	return (
+		<div>
+			<h1>desde el banner</h1>
+			<h1>desde el banner</h1>
+			<h1>desde el banner</h1>
+			<h1>desde el banner</h1>
+			<h1>desde el banner</h1>
+			<h1>desde el banner</h1>
+		</div>
+	);
+};
