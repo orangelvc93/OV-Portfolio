4b81bcb7d7fea6d9d072ed6e492ad9c621602b3b
Orangel Valdespino <orangelvc93@gmail.com>
2024-04-04 18:55:15 -0500

    primer commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,53 @@
 import React from "react";
 import "../styles/Banner.css";
+import perfil from "../../public/img/Perfil.jpg";
+
+import { FaLinkedin, FaGithub, FaInstagram, FaFacebook } from "react-icons/fa6";
 
 export const Banner = () => {
 	return (
-		<div>
-			<h1>desde el banner</h1>
-			<h1>desde el banner</h1>
-			<h1>desde el banner</h1>
-			<h1>desde el banner</h1>
-			<h1>desde el banner</h1>
-			<h1>desde el banner</h1>
-		</div>
+		<section className="banner-container">
+			<div className="banner-content">
+				<h2>Build Digital Experiences that inspire</h2>
+				<p>
+					Passionate Frontend Developer | Transforming ideas into seamless and
+					visually stunning Web solutions.
+				</p>
+			</div>
+
+			<div className="social-wrap">
+				<a
+					href="#"
+					className="social-icon"
+				>
+					<FaLinkedin />
+				</a>
+				<a
+					href="#"
+					className="social-icon"
+				>
+					<FaGithub />
+				</a>
+				<a
+					href="#"
+					className="social-icon"
+				>
+					<FaInstagram />
+				</a>
+				<a
+					href="#"
+					className="social-icon"
+				>
+					<FaFacebook />
+				</a>
+			</div>
+
+			<div className="banner-profile">
+				<img
+					src={perfil}
+					alt="Imagen de perfil"
+				/>
+			</div>
+		</section>
 	);
 };
